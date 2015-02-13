2e3127a5981921b99d0214d5073a3cc94600088b
Ishan Bhutani <ishan.b@live.ca>
2015-02-13 12:07:15 -0800

    Added animation to player

--- EARTHBOUND/Assets/Scripts/Player/Player.cs
+++ EARTHBOUND/Assets/Scripts/Player/Player.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	public float acceleration = 0.5f;
-	public float maxSpeed = 2.5f;
+	public float acceleration = 5f;
+	public float maxSpeed = 10f;
+
+	private Animator animator;
+	private const string animState = "AnimState"; //This should probably be a constant somewhere throughout app
 
 	// Use this for initialization
 	void Start () {
-	
+		animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -21,13 +24,13 @@ public class Player : MonoBehaviour {
 
 		if(Input.GetKey(KeyCode.RightArrow)) {
 			if(currentVelocityX < maxSpeed) {
-				accelerationX += acceleration;
+				accelerationX = acceleration;
 			}
 		}
 
 		if(Input.GetKey(KeyCode.LeftArrow)) {
 			if(currentVelocityX > -maxSpeed) {
-				accelerationX -= acceleration;
+				accelerationX = -acceleration;
 			}
 		}
 
@@ -35,7 +38,7 @@ public class Player : MonoBehaviour {
 			// dont know what to do here
 		}
 
-		if(Input.GetKey(KeyCode.UpArrow)) {
+		if(Input.GetKeyUp(KeyCode.UpArrow)) {
 			//jump? jump twice?
 		}
 
@@ -45,7 +48,19 @@ public class Player : MonoBehaviour {
 	private void ApplyForce(float forceX, float forceY) {
 		if (forceX != 0) {
 			transform.localScale = new Vector3 (forceX > 0 ? 1 : -1, 1);
+			animator.SetInteger (animState, AnimationState.PlayerWalkAnimation.GetHashCode ());
+		} else {
+			animator.SetInteger(animState, AnimationState.PlayerIdleAnimation.GetHashCode());
 		}
+
 		rigidbody2D.AddForce(new Vector2(forceX, forceY));
+
+		print (rigidbody2D.velocity);
 	}
+}
+
+//Should map to AnimState in animator for Player object
+public enum AnimationState {
+	PlayerIdleAnimation = 0,
+	PlayerWalkAnimation = 1
 }
