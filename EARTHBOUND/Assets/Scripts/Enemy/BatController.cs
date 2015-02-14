c49a9fc74ba53c6662a86aae08f44f633c28de1d
Jason Chong <jrchong@sfu.ca>
2015-02-13 16:43:40 -0800

    Added bat

---
+++ EARTHBOUND/Assets/Scripts/Enemy/BatController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatController : MonoBehaviour {
+
+	public float xVelocity;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (new Vector3(xVelocity, 0));
+	}
+
+	void OnTriggerEnter2D( Collider2D collidedObject )
+	{   if (collidedObject.name.Equals ("Player")) {
+			Debug.LogError ("asdf");
+		}
+	}  
+}
